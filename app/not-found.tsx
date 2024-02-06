692b3996f9629f511455ff6efd339382ef99eaa6
Dmitriy Belkin <demetrius.belkin@gmail.com>
2024-02-06 15:18:00 +0300

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,18 @@
-import Link from '@/components/Link'
+// import Link from '@/components/Link'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          {/* <img src="/static/images/404.png" /> */}
+          <Image 
+            src="/static/images/404.png"
+            width={658}
+            height={494}
+            alt="404 status code"
+          />
         </h1>
       </div>
       <div className="max-w-md">
@@ -13,9 +20,9 @@ export default function NotFound() {
           Sorry we couldn't find this page.
         </p>
         <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-        {/* <Link href="/" className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+        <Link href="/" className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
           Back to homepage
-        </Link> */}
+        </Link>
       </div>
     </div>
   )
